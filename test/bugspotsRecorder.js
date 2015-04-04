0b3440690bb5e1b96711d8364abb58114b9bb355
thejsf <joe.forbes@gmail.com>
2015-04-03 21:48:22 -0500

    Got mocks and such working.

---
+++ test/bugspotsRecorder.js
@@ -0,0 +1,4 @@
+/**
+ * Created by joe on 4/3/15.
+ */
+/* Sets up logging to record inputs and outputs for a given configuration. */
--- test/bugspotsTests.js
+++ test/bugspotsTests.js
@@ -1,64 +1,86 @@
 /**
  * Created by joe on 4/1/15.
  */
-var nockExec = require('nock-exec');
 var logger = require('../util/logger');
-var testCommits = require('../resources/test/testCommits.json');
 
 logger.addTarget({
   targetType: 'file', targetConfig: {
     level: 'debug',
     filename: './test.log',
     handleExceptions: true,
-    json: true,
+    json: false,
     maxsize: 5242880, // 5MB
     maxFiles: 5,
     colorize: false
   }
 });
 
-logger.addTarget({
-  targetType: 'console'
-});
+//logger.addTarget({targetType: 'console'});
 
-testCommits.forEach(function(testCommit) {
-  testCommit.parents = function() {
-    return testCommit.parentCommits;
-  };
-  testCommit.committed_date = new Date(testCommit.committed_date);
+var testResources = require('../resources/test/bugspotsTestResources2.json');
+
+testResources.commitSets.forEach(function (commitSet) {
+  commitSet.forEach(function (testCommit) {
+    testCommit.committed_date = new Date(testCommit.committed_date);
+    testCommit.parents = function () {
+      return testCommit.parentCommits;
+    }
+  });
 });
 
 var proxyquire = require('proxyquire');
 
-var giftStub = function() {
-  //what's needed here?
+var commitSetIndex = -1;
+
+var giftStub = function () {
   return {
-    branch: function(branch, headCommitHandler){
-      headCommitHandler(null, {commit: testCommits[0]});
+    branch: function (branch, headCommitHandler) {
+      headCommitHandler(null, testResources.branch);
     },
 
-    commits: function(commitId, depthToRetrieve, commitListHandler){
-      commitListHandler(null, testCommits);
+    commits: function (commitId, depthToRetrieve, commitListHandler) {
+      commitSetIndex++;
+      commitListHandler(null, testResources.commitSets[commitSetIndex]);
     }
-  };
+  }
+};
 
+var childProcessStub = {
+  exec: function (cmd, opt, callback) {
+    function go() {
+      logger.debug('processing command: ' + cmd);
+      callback(null, testResources.diffCommands[cmd].replace(/\\n/g, '\n'));
+    }
+    setTimeout(go, 100);
+  }
 };
 
 var Bugspots = proxyquire('../lib/bugspots', {
-  'gift': giftStub
+  'gift': giftStub,
+   'child_process': childProcessStub
 });
 
-nockExec('git diff commit1..commit2 --name-only').reply(0, 'file1\n');
-nockExec('echo \'\'').reply(0, '\n');
-nockExec('echo \'\'').reply(0, '\n');
-nockExec('echo \'\'').reply(0, '\n');
+describe('bugspots tests', function () {
 
-describe('bugspots tests', function() {
-
-  it('should not blow up when you call it.', function(done){
+  it('should not blow up when you call it.', function (done) {
     var scanner = new Bugspots();
 
-    var options = {depth: 2};
+    var options = {
+      repo: '/home/joe/git/github/django/django-trunk',
+      branch: 'stable/1.7.x',
+      regex: /\b(fix(es|ed)?|close(s|d)?)\b/i,
+      useRelativeDates: true,
+      markerCommitId: '159f3bfafced8b546010caeaafabecf735598e34', //1.7.7
+//  markerCommitId: '40fb8f4ecd740cbfc2b2c3651d69cbbb3cc2506b', //1.7.6
+//  markerCommitId: '634f4229c5cafeb3a1c03e5deb9434d7c0f74ebe', //1.7.5
+//  markerCommitId: 'b626c289ccf9cc487f97d91c2a45cac096d9d0c7', //1.7.4
+//  markerCommitId: '6bf1930fb5c7c6a47992ff368e21c58f4f14b402', //1.7.3
+//  markerCommitId: '880d7638cf66ed28a60b62335ccfc5dfd5052937', //1.7.2
+//  markerCommitId: 'c5780adeecfbd85a80b5aa7130dd86e78b23e497', //1.7.1
+//  markerCommitId: 'd92b08536d873c0966e8192e64d8e8bd9de79ebe', //1.7
+      depth: 10,
+      batchSize: 5
+    };
 
     var processResults = function (err, hotspots) {
       if (err) {
@@ -66,7 +88,8 @@ describe('bugspots tests', function() {
         return;
       }
       hotspots.forEach(function (hotspot) {
-        console.log(hotspot.file  + '\t' + hotspot.fixCommits + '\t' + hotspot.firstCommit.toISOString() + '\t' + hotspot.lastCommit.toISOString() + '\t' + hotspot.score);
+        logger.debug(JSON.stringify(hotspot));
+        console.log(hotspot.file + '\t' + hotspot.fixCommits + '\t' + hotspot.firstCommit.toISOString() + '\t' + hotspot.lastCommit.toISOString() + '\t' + hotspot.score);
       });
       done();
     };
