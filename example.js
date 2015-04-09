815e63e432fa5c69247ed350fd0d5f5f1be846df
thejsf <joe.forbes@gmail.com>
2015-04-08 23:29:06 -0500

    Trying to match upstream

--- example.js
+++ example.js
@@ -2,9 +2,9 @@
 
 var BugSpots = require('./index')
 
-var z = new BugSpots();
+var z = new BugSpots()
 
 z.scan({ repo: process.argv[2] }, function cb(err, hotspots) {
   if (err) { console.log(err) }
   else { console.log('hotspots: ', hotspots) }
-});
+})
