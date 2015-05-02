f07b4fda216fdf5e179afb7b0d9b0c3d6add2f81
thejsf <joe.forbes@gmail.com>
2015-05-02 10:07:59 -0500

    JSCS linting.

--- test/testResourcesLoader.js
+++ test/testResourcesLoader.js
@@ -1,37 +1,70 @@
+/**
+ * @typedef {object} TestCommit
+ * @type {object}
+ * @property {string[]} parentCommits
+ * @property {date|string} committed_date
+ * @property {function} parents
+ */
+
+/**
+ * @typedef {object} TestResources
+ * @property {object} testCases
+ * @property {object[]} commitArray
+ * @property {object} commitLookup
+ */
+
+/**
+ * @typedef {object} TestCase
+ * @property {object} regexSpec
+ * @property {object} options
+ */
+
 var testResourcesLoader = {
-  load: function (file) {
-    var testResources = require(file);
+    load: function (file) {
+        /** @type {TestResources} */
+        var testResources = require(file),
+            names,
+            i,
+            /** @type {TestCase} */
+            testCase,
+            pattern,
+            options;
 
-    testResources.commitArray = [];
-    testResources.commitLookup = {};
+        testResources.commitArray = [];
+        testResources.commitLookup = {};
 
-    testResources.commits.forEach(function (testCommit) {
-      testResources.commitArray.push(testCommit);
-      testResources.commitLookup[testCommit.id] = testResources.commitArray.length - 1;
-      testCommit.committed_date = new Date(testCommit.committed_date);
-      testCommit.parents = function () {
-        return testCommit.parentCommits;
-      }
-    });
+        testResources.commits.forEach(
+            /**
+             * @param {TestCommit} testCommit
+             */
+            function (testCommit) {
+                testResources.commitArray.push(testCommit);
+                testResources.commitLookup[testCommit.id] = testResources.commitArray.length - 1;
+                testCommit.committed_date = new Date(testCommit.committed_date);
+                testCommit.parents = function () {
+                    return testCommit.parentCommits;
+                }
+            }
+        );
 
-    var names = Object.getOwnPropertyNames(testResources.testCases);
-    for (var i = 0; i < names.length; i++) {
-      var testCase = testResources.testCases[names[i]];
-      testCase.hotspots.forEach(function (hotspot) {
-        hotspot.firstCommit = new Date(hotspot.firstCommit);
-        hotspot.lastCommit = new Date(hotspot.lastCommit);
-      });
+        names = Object.getOwnPropertyNames(testResources.testCases);
+        for (i = 0; i < names.length; i++) {
+            testCase = testResources.testCases[names[i]];
+            testCase.hotspots.forEach(function (hotspot) {
+                hotspot.firstCommit = new Date(hotspot.firstCommit);
+                hotspot.lastCommit = new Date(hotspot.lastCommit);
+            });
 
-      if (testCase.options.hasOwnProperty('regexSpec')) {
-        var pattern = testCase.options.regexSpec.pattern,
-          options = testCase.options.regexSpec.options;
-        testCase.options.regex = new RegExp(pattern, options);
-      }
+            if (testCase.options.hasOwnProperty('regexSpec')) {
+                pattern = testCase.options.regexSpec.pattern;
+                options = testCase.options.regexSpec.options;
+                testCase.options.regex = new RegExp(pattern, options);
+            }
 
+        }
+
+        return testResources;
     }
-
-    return testResources;
-  }
 };
 
 module.exports = testResourcesLoader;
