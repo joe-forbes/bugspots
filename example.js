02f43df3f8e40776ce7e22e0e96d4d10fb019edb
thejsf <joe.forbes@gmail.com>
2015-05-02 11:13:13 -0500

    JSCS linting.

--- example.js
+++ example.js
@@ -1,8 +1,10 @@
-var BugSpots = require('./index');
+var BugSpots = require('./index'),
+    z = new BugSpots();
 
-var z = new BugSpots();
-
-z.scan({ repo: process.argv[2] }, function cb(err, hotspots) {
-  if (err) { console.log(err) }
-  else { console.log('hotspots: ', hotspots) }
+z.scan({repo: process.argv[2]}, function cb(err, hotspots) {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log('hotspots: ', hotspots)
+    }
 });
